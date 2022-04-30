d578b7b0ea5a8865431de0d5a18e70d1b46a3cec
matthewru <matthewru2005@gmail.com>
2022-04-30 15:44:09 -0500

    moho please work

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import pygame
 
 pygame.display.init()
 
-screen = pygame.display.set_mode((300, 300))
+screen = pygame.display.set_mode((1000, 600))
 
-screen.fill((234, 212, 252))
+screen.fill((230, 230, 230))
+
+pygame.display.flip()
+
+running = True
+while running:
+    
+# for loop through the event queue  
+    for event in pygame.event.get():
+      
+        # Check for QUIT event      
+        if event.type == pygame.QUIT:
+            running = False
